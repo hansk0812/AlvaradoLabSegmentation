f53597d41e9674ab0122f34c4a3a94a336547021
hans <hanskrupakar1994@gmail.com>
2023-04-11 03:36:26 +0530

    converted-into-importable-package

---
+++ ecology_semantic_segmentation/dataset/fish/fish_segmentation.py
@@ -0,0 +1,167 @@
+import os
+import glob
+import traceback
+
+import rawpy
+import cv2
+import numpy as np
+
+from torch.utils.data import Dataset
+
+from . import composite_labels
+
+from ..augment import augment_fn
+
+def imread(file_path):
+    
+    if ".arw" not in file_path.lower():
+        return cv2.imread(file_path)
+    else:
+        img = rawpy.imread(file_path) 
+        img = img.postprocess() 
+        return cv2.cvtColor(img, cv2.COLOR_RGB2BGR) 
+
+class SegmentationDataset(Dataset):
+
+    def __init__(self, segmentation_data, img_shape, min_segment_positivity_ratio, sample_dataset = True, organs=None): 
+        
+        if sample_dataset:
+            segmentation_data = {key: segmentation_data[key] for key in list(segmentation_data)[:60]}
+        
+        # Ensure all files contribute to data wrt organs
+        if organs is None:
+            test_organs = composite_labels
+        else:
+            test_organs = organs
+
+        removable_keys = []
+        for key in segmentation_data:
+            
+            ctx = 0
+            for organ in test_organs:
+                try:
+                    imread(segmentation_data[key]["segments"][organ])
+                    ctx += 1
+                except Exception:
+                    continue
+            
+            if ctx == 0:
+                removable_keys.append(key)
+        
+        for key in removable_keys:
+            del segmentation_data[key]
+
+        self.segmentation_data = segmentation_data
+        self.segmentation_keys = list(segmentation_data.keys())
+        self.img_shape = img_shape 
+        self.min_segment_positivity_ratio = min_segment_positivity_ratio
+        self.organs = organs
+        
+        if not organs is None:
+            self.label_indices = sorted([composite_labels.index(organ) for organ in organs])
+        else:
+            self.label_indices = list(range(len(composite_labels)))
+
+    def __len__(self):
+        return len(self.segmentation_keys)
+
+    def __getitem__(self, idx):
+        
+        data_key = self.segmentation_keys[idx]
+        
+        image_path, segments_paths = self.segmentation_data[data_key]["image"], self.segmentation_data[data_key]["segments"]
+        image = imread(image_path)
+        image = cv2.resize(image, (self.img_shape, self.img_shape))
+        
+        num_segments = len(composite_labels) if self.organs is None else len(self.organs)
+        segment_array = np.zeros((self.img_shape, self.img_shape, num_segments)) 
+        
+        for organ_index in self.label_indices:
+            
+            try:
+                organ = composite_labels[organ_index]
+                segment = imread(segments_paths[organ])
+            
+                segment = cv2.resize(segment, (self.img_shape, self.img_shape))
+                segment = cv2.cvtColor(segment, cv2.COLOR_BGR2GRAY)
+                segment[segment > 245] = 0
+                segment[segment != 0] = 255
+                
+                print (segment.min(), segment.max())
+                cv2.imwrite("f.png", segment); 
+                
+                area_of_segment = segment.sum() / 255.0
+                
+                if area_of_segment * 255 < (self.min_segment_positivity_ratio * self.img_shape * self.img_shape):
+                    #TODO: Ignore labels
+                    segment.fill(0) # (-1)
+                
+                segment_array[:, :, organ_index] = segment 
+            
+            except Exception:
+                traceback.print_exc()
+                segment_array[:, :, organ_index].fill(-1) 
+        
+        image, segment_array = augment_fn(image, segment_array)
+
+        return image.transpose((2,0,1)).astype(np.float32), segment_array.transpose((2,0,1)).astype(np.float32), segments_paths[organ]
+
+def get_ml_training_set_data(dtype, path, folder_path, img_shape, min_segment_positivity_ratio, sample_dataset=True, organs=None):
+    
+    #TODO: 9 missing images from dataset!
+
+    global composite_labels 
+
+    assert dtype == "segmentation/composite"
+    
+    folders = [x for x in glob.glob(os.path.join(folder_path, path, "*")) \
+                if os.path.isdir(x)]
+    
+    data = {}
+    for directory in folders:
+        
+        dir_folders = glob.glob(os.path.join(directory, "*"))
+        
+        images = glob.glob(os.path.join(directory, 'original image/*'))
+        for image_path in images:
+            fname = "/".join(image_path.split('/')[-3:])
+            search_key = '.'.join(fname.split('/')[-1].split('.')[:-1])
+            data_index = os.path.join(directory.split('/')[-1], search_key)
+            
+            segments_path = glob.glob(os.path.join(directory, "*", search_key + "*"))
+            segments = [x.split('/')[-2] for x in segments_path]
+            segments.remove("original image")
+            
+            if not os.path.exists(image_path):
+                #TODO print (image_path)
+                continue
+
+            segment_paths = {}
+            for organ in segments:
+                ann_paths = glob.glob(os.path.join(directory, organ, search_key + "*")) 
+                
+                organ = organ.replace(" ", "_")
+                if not organ in composite_labels:
+                    composite_labels.append(organ)
+
+                if len(ann_paths) == 1:
+                    if os.path.exists(ann_paths[0]):
+                        segment_paths[organ] = ann_paths[0]  
+            
+            if len(segment_paths) > 0:
+
+                try:
+                    img = cv2.imread(image_path)
+                    assert not img is None
+
+                    data[data_index] = {"image": image_path, \
+                                        "segments": segment_paths}
+
+                except Exception:
+                    pass
+
+    dataset = SegmentationDataset(data, img_shape, min_segment_positivity_ratio, sample_dataset=sample_dataset, organs=organs)
+    print ("Using %d labeled images from dataset: %s!" % (len(dataset), "Segmentation dataset: %s" % path))
+    
+    return dataset
+
---
+++ ecology_semantic_segmentation/loss_functions.py
@@ -0,0 +1,72 @@
+from torch.nn import functional as F
+import numpy as np
+
+import torch
+from torch import nn
+
+logsoftmax = lambda x: F.log_softmax(x, dim=2)
+
+def cross_entropy_loss(gt, pred, weight=0.3, bce=False):
+
+    if not bce:
+        ce = F.cross_entropy(pred[:,0,:], torch.argmax(gt[:,0,:], dim=1)) 
+    else:
+        #ce = F.binary_cross_entropy(pred, gt) #, weight=torch.tensor([1-weight, weight]).cuda())
+        ce = - (1-weight) * (gt*logsoftmax(pred)) - weight * (1-gt)*logsoftmax(1-pred)
+        ce *= 1e-5
+    return torch.sum(ce)
+
+def focal_loss(gt, pred, gamma=1.5, factor=0.1):
+    fl = - gt*torch.pow(pred, gamma)*logsoftmax(pred) - (1-gt)*torch.pow(1-pred, gamma)*logsoftmax(1-pred)
+    #print (fl)
+    return factor * torch.sum(fl)
+
+def dice_loss(gt, pred, generalized=False):
+
+    if not generalized:
+        dl_n = 2 * torch.sum(gt * pred)
+        dl_d = torch.sum(gt * gt) + torch.sum(pred * pred)
+    
+        return 1.0 - (dl_n + 1e-7) / (dl_d + 1e-7)
+    else:
+        # dc = (\sum p*gt + \eta) / (\sum p + \sum gt + \eta) + \
+        #       (\sum (1-p)*(1-gt) + \eta) / ((\sum (1-p) + \sum (1-gt)) + \eta)
+        # dice_loss = 1 - dc
+        
+        G1, P1 = gt, pred 
+        G0, P0 = (1-gt), (1-pred)
+
+        dice_coeff_preds_fg = torch.sum(G1 * P1) + 1e-7
+        dice_coeff_preds_bg = torch.sum(G0 * P0) + 1e-7
+        dice_coeff_normalize_fg = torch.sum(G1) + torch.sum(P1) + 1e-7
+        dice_coeff_normalize_bg = torch.sum(G0) + torch.sum(P0) + 1e-7
+        
+        dc = (dice_coeff_preds_fg / dice_coeff_normalize_fg) + (dice_coeff_preds_bg / dice_coeff_normalize_bg)
+        
+        return 1.0 - dc
+
+def twersky_loss(gt, pred, alpha=0.5, beta=0.3):
+
+    tl_n = torch.sum(gt * pred) 
+    tl_d = torch.sum(gt * pred) + alpha * torch.sum((1-pred)*gt) + beta * torch.sum(pred*(1-gt))
+
+    return 1.0 - (tl_n + 1e-7) / (tl_d + 1e-7)
+
+def focal_dice_coefficient(gt, pred, alpha=0.5, beta=0.3, gamma=1.8):
+
+    fdsc_n = 2 * torch.sum(gt * torch.pow(1-pred, gamma) * pred) 
+    fdsc_d = torch.sum(gt * torch.pow(1-pred, gamma) * pred) + \
+             torch.sum((1-gt) * torch.pow(1-pred, alpha) * pred) + \
+             torch.sum((1-pred) * torch.pow(pred, alpha) * gt)
+
+    return 1.0 - (fdsc_n + 1e-7) / (fdsc_d + 1e-7)
+
+
+def classification_dice_loss(gt, pred, factor=1e3):
+    
+    dice_l = dice_loss(gt, pred)
+    generalized_dice_l = dice_loss(gt, pred, generalized=True)
+    twersky_l = twersky_loss(gt, pred)
+    focal_dice_l = focal_dice_coefficient(gt, pred) 
+    m = factor * 0.33 
+    return  dice_l*m, generalized_dice_l*m, twersky_l*m, focal_dice_l*m
