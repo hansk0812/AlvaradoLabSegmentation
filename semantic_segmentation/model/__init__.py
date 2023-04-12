83639a44e72c309559fb4b5a4070244ea4697591
hans <hanskrupakar1994@gmail.com>
2023-04-12 08:48:23 +0530

    added-config-file

--- semantic_segmentation/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from .vgg import VGGUNet
-from .vgg import vgg19_bn as VGGClassifier, VGG19_BN_Weights
-
-vgg_classifier = VGGClassifier(weights=VGG19_BN_Weights.DEFAULT)
-vgg_unet = VGGUNet(vgg_classifier)
-
-__all__ = ["vgg_unet"]
