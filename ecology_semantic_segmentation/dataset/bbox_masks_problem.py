d1addbe0c7b83e744dd12af2ef6d54b8c27fef1b
hans <hanskrupakar1994@gmail.com>
2023-04-22 15:45:27 +0530

    minor-changes-and-relative-imports

--- ecology_semantic_segmentation/dataset/bbox_masks_problem.py
+++ ecology_semantic_segmentation/dataset/bbox_masks_problem.py
@@ -2,11 +2,28 @@ import cv2
 import time
 import numpy as np
 
-from .fish import fish_train_dataset, fish_val_dataset, fish_test_dataset
 from .manual_bbox_identification import return_bbox_corrected_annotations
 
 # Found small artifacts inside segmentation file of "whole body"!
 # 12-23-2019/original image/f99C.jpg
+
+def remove_islands_in_segment_gt(mask):
+    
+    contours, hierarchy = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    if len(contours) > 1:
+        large_enough_contours = []
+        for idx, contour in enumerate(contours):
+            area = shoelace_algorithm(contour)
+            
+            # Remove random spots on mask based on contour area
+            if area > 1000:
+                large_enough_contours.append(contour)
+        
+        mask = np.zeros_like(mask)
+        mask = cv2.drawContours(mask, large_enough_contours, -1, 1, -1)
+
+    return mask
 
 def shoelace_algorithm(contour):
 
@@ -20,19 +37,7 @@ def shoelace_algorithm(contour):
 
 def get_bounding_box_from_mask(mask):
     
-    contours, hierarchy = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
-    if len(contours) > 1:
-        large_enough_contours = []
-        for idx, contour in enumerate(contours):
-            area = shoelace_algorithm(contour)
-            
-            # Remove random spots on mask based on contour area
-            if area > 1000:
-                large_enough_contours.append(contour)
-        
-        mask = np.zeros_like(mask)
-        mask = cv2.drawContours(mask, large_enough_contours, -1, 255, -1)
+    mask = remove_islands_in_segment_gt(mask)
 
     maskx = np.any(mask, axis=0)
     masky = np.any(mask, axis=1)
@@ -46,15 +51,19 @@ def get_bounding_box_from_mask(mask):
     return (x1, y1), (x2, y2)
     # return (xmin, ymin), (xmax, ymax)
 
-# Display all files that need to be moved to new dataset folder
-for img, ann, fname in fish_train_dataset:
+if __name__ == "__main__":
 
-    ann[ann>0] = 1
-    ann = ann[0]
+    from .fish import fish_train_dataset, fish_val_dataset, fish_test_dataset
     
-    (x1, y1), (x2, y2) = get_bounding_box_from_mask(ann)
-    
-    threshold=5
-    if abs((x2 - x1) - ann.shape[0]) +  abs((y2-y1) - ann.shape[1]) < 2*threshold:
-        print (fname)
+    # Display all files that need to be moved to new dataset folder
+    for img, ann, fname in fish_train_dataset:
+
+        ann[ann>0] = 1
+        ann = ann[0]
+        
+        (x1, y1), (x2, y2) = get_bounding_box_from_mask(ann)
+        
+        threshold=5
+        if abs((x2 - x1) - ann.shape[0]) +  abs((y2-y1) - ann.shape[1]) < 2*threshold:
+            print (fname)
 
