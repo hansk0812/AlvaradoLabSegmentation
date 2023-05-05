9de54afe752d88c52f2ab1f33d93bcaa84a4f959
hans <hanskrupakar1994@gmail.com>
2023-05-05 12:02:00 +0530

    test-script-for-model-selection-based-on-test-set

--- ecology_semantic_segmentation/dataset/fish/__init__.py
+++ ecology_semantic_segmentation/dataset/fish/__init__.py
@@ -17,7 +17,7 @@ except Exception:
     IMG_SIZE = 256
 deepsupervision = False
 
-fish_train_dataset = FishDataset(dataset_type=["segmentation", "segmentation/composite"], 
+fish_train_dataset = FishDataset(dataset_type=["segmentation/composite", "segmentation"], 
                                  img_shape=IMG_SIZE, 
                                  sample_dataset=SAMPLE_DATASET,
                                  deepsupervision=deepsupervision)
