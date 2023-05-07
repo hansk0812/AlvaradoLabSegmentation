170c7f0e1de723c4f8cc9ed4a96ca0c88e837d01
hans <hanskrupakar1994@gmail.com>
2023-05-07 14:44:12 +0530

    changed-repo-readme

--- ecology_semantic_segmentation/train.py
+++ ecology_semantic_segmentation/train.py
@@ -213,16 +213,16 @@ def load_recent_model(saved_dir, net):
         traceback.print_exc()
         return -1
 
+import segmentation_models_pytorch as smp
+vgg_unet = smp.Unet(
+            encoder_name="resnet50",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=1,                      # model output channels (number of classes in your dataset)
+        )
+
 if __name__ == "__main__":
     
-    import segmentation_models_pytorch as smp
-    vgg_unet = smp.Unet(
-                encoder_name="resnet50",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-                encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
-                in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-                classes=1,                      # model output channels (number of classes in your dataset)
-            )
-
     #TODO Discretized image sizes to closest multiple of 8
    
     ap = argparse.ArgumentParser()
