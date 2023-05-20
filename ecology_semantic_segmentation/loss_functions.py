199a0eed33560ff4add9e73af265afb32b6df226
hans <hanskrupakar1994@gmail.com>
2023-05-20 17:43:31 +0530

    this-is-the-formula-will-i-am-song

--- ecology_semantic_segmentation/loss_functions.py
+++ ecology_semantic_segmentation/loss_functions.py
@@ -1,5 +1,3 @@
-BACKGROUND_FLAG = False
-
 from torch.nn import functional as F
 import numpy as np
 
@@ -29,12 +27,13 @@ def cross_entropy_loss(gt, pred, weight=0.3, bce=False, background_weight = 0):
 
     if not bce:
         ce = F.cross_entropy(pred, gt) 
+        ce += background_weight * F.cross_entropy(1-pred, 1-gt)
     else:   
         
         # more stable
         ce = binary_cross_entropy(pred, gt) 
         
-        # one vs all loss functions for conceptual loss definition
+        # one vs all background losses drone ignores BCE definition because he lives in India
         #ce += background_weight * binary_cross_entropy(1-pred, 1-gt)
 
         #eps = 1e-5
@@ -46,9 +45,7 @@ def cross_entropy_loss(gt, pred, weight=0.3, bce=False, background_weight = 0):
 
 def focal_loss(gt, pred, gamma=1.5, factor=0.1, background_weight=0):
     fl = - torch.pow((1-pred), gamma)*torch.log(pred+1e-7)
-    
-    if BACKGROUND_FLAG:
-        fl += - background_weight * torch.pow(pred, gamma) * torch.log(1-pred+1e-7)
+    fl += - background_weight * torch.pow(pred, gamma) * torch.log(1-pred+1e-7)
 
     return factor * torch.mean(fl)
 
@@ -58,15 +55,12 @@ def dice_loss(gt, pred, generalized=False, background_weight = 1):
         dl_n = 2 * torch.sum(gt * pred)
         dl_d = torch.sum(gt + pred * pred) # gt * gt = gt
         dice_fg = (dl_n + 1e-7) / (dl_d + 1e-7) 
-     
-        if BACKGROUND_FLAG:
-            l_bg_n = 2 * torch.sum((1-gt)*(1-pred))
-            l_bg_d = 2 * torch.sum((1-gt) + (1-pred)*(1-pred)) # 1-gt * 1-gt = 1-gt
-            ice_bg = (dl_bg_n + 1e-7) / (dl_bg_d + 1e-7) 
-            return - dice_fg - background_weight * dice_bg
         
-        return - dice_fg
+        dl_bg_n = 2 * torch.sum((1-gt)*(1-pred))
+        dl_bg_d = 2 * torch.sum((1-gt) + (1-pred)*(1-pred)) # 1-gt * 1-gt = 1-gt
+        dice_bg = (dl_bg_n + 1e-7) / (dl_bg_d + 1e-7) 
 
+        return - dice_fg - background_weight * dice_bg
     else:
         # dc = (\sum p*gt + \eta) / (\sum p + \sum gt + \eta) + \
         #       (\sum (1-p)*(1-gt) + \eta) / ((\sum (1-p) + \sum (1-gt)) + \eta)
@@ -78,11 +72,10 @@ def dice_loss(gt, pred, generalized=False, background_weight = 1):
         dice_coeff_preds_fg = torch.sum(G1 * P1) + 1e-7
         dice_coeff_normalize_fg = torch.sum(G1 + P1*P1) + 1e-7 # G1*G1 = G1
         dc = (dice_coeff_preds_fg / dice_coeff_normalize_fg) 
-        
-        if BACKGROUND_FLAG:
-            dice_coeff_preds_bg = torch.sum(G0 * P0) + 1e-7
-            dice_coeff_normalize_bg = torch.sum(G0 + P0*P0) + 1e-7 # G0*G0 = G0
-            dc += background_weight * (dice_coeff_preds_bg / dice_coeff_normalize_bg)
+
+        dice_coeff_preds_bg = torch.sum(G0 * P0) + 1e-7
+        dice_coeff_normalize_bg = torch.sum(G0 + P0*P0) + 1e-7 # G0*G0 = G0
+        dc += background_weight * (dice_coeff_preds_bg / dice_coeff_normalize_bg)
         
         return - dc
 
@@ -92,15 +85,13 @@ def twersky_loss(gt, pred, alpha=0.5, beta=0.3, background_weight=0):
     tl_d = torch.sum(gt * pred) + alpha * torch.sum((1-pred)*gt) + beta * torch.sum(pred*(1-gt))
     td_fg = - (tl_n + 1e-7) / (tl_d + 1e-7)
     
-    if BACKGROUND_FLAG:
-        gt = 1-gt
-        pred = 1-pred
-        tl_bg_n = torch.sum(gt * pred) 
-        tl_bg_d = torch.sum(gt * pred) + alpha * torch.sum((1-pred)*(gt)) + beta * torch.sum(pred*(1-gt))
-        td_bg = - (tl_bg_n + 1e-7) / (tl_bg_d + 1e-7)
-        return td_fg + background_weight * td_bg
-    
-    return td_fg
+    gt = 1-gt
+    pred = 1-pred
+    tl_bg_n = torch.sum(gt * pred) 
+    tl_bg_d = torch.sum(gt * pred) + alpha * torch.sum((1-pred)*(gt)) + beta * torch.sum(pred*(1-gt))
+    td_bg = - (tl_bg_n + 1e-7) / (tl_bg_d + 1e-7)
+
+    return td_fg + background_weight * td_bg
 
 def focal_dice_coefficient(gt, pred, alpha=0.5, beta=0.3, gamma=1.8, background_weight=0):
 
@@ -109,14 +100,12 @@ def focal_dice_coefficient(gt, pred, alpha=0.5, beta=0.3, gamma=1.8, background_
     dice_coeff_fg = (dl_n + 1e-7) / (dl_d + 1e-7)
     fg_dice = - torch.pow(1-dice_coeff_fg, gamma) * torch.log(dice_coeff_fg + 1e-7)
 
-    if BACKGROUND_FLAG:
-        dl_bg_n = 2 * torch.sum((1-gt) * (1-pred))
-        dl_bg_d = torch.sum((1-gt) + (1-pred) * (1-pred)) # 1-gt * 1-gt
-        dice_coeff_bg = (dl_bg_n + 1e-7) / (dl_bg_d + 1e-7)
-        bg_dice = - torch.pow(1-dice_coeff_bg, gamma) * torch.log(dice_coeff_bg + 1e-7)
-        return fg_dice + background_weight * bg_dice
+    dl_bg_n = 2 * torch.sum((1-gt) * (1-pred))
+    dl_bg_d = torch.sum((1-gt) + (1-pred) * (1-pred)) # 1-gt * 1-gt
+    dice_coeff_bg = (dl_bg_n + 1e-7) / (dl_bg_d + 1e-7)
+    bg_dice = - torch.pow(1-dice_coeff_bg, gamma) * torch.log(dice_coeff_bg + 1e-7)
 
-    return fg_dice
+    return fg_dice + background_weight * bg_dice
 
 def classification_dice_loss(gt, pred, factor=1e3, background_weight=1):
     
