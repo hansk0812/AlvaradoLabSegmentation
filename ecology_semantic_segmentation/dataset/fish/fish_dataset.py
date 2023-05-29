f2809d5ddd666dc7654a85d713b4a3467de0d168
hans <hanskrupakar1994@gmail.com>
2023-05-29 12:23:44 +0530

    strongest-of-the-pack-initial-code

--- ecology_semantic_segmentation/dataset/fish/fish_dataset.py
+++ ecology_semantic_segmentation/dataset/fish/fish_dataset.py
@@ -128,6 +128,13 @@ class FishDataset(Dataset):
 
     def __len__(self):
         return self.dataset_cumsum_lengths[-1]
+    
+    def return_descending_order_sets(self, img, ann):
+        # Preferred order: easiest to segment organ as ann[0] --> hardest to segment as ann[-1]
+        for idx in range(ann.shape[0]-1):
+            ann[idx] = sum(x for x in ann[idx+1:])
+        ann[ann>1] = 1
+        return ann
 
     def __getitem__(self, idx):
         
