9472676af33cb41323c8c4820ec248d53b66a84b
hans <hanskrupakar1994@gmail.com>
2023-07-06 11:47:00 +0530

    removed-lfs

--- remove.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-
-print (os.path.join("models", os.environ["EXPTNAME"], "channels256/img256/*"))
-
-#
-for models in sorted(glob.glob(os.path.join("models", os.environ["EXPTNAME"], "channels256/img256/*")), key=lambda x: int(x.split('.pt')[0].split('epoch')[-1]))[-1:]:
-    print (models) 
-    os.remove(models)
