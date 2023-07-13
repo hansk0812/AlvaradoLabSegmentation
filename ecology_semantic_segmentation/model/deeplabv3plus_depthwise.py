ba71ddfac4ea109e747dfc6c749510986ae766b0
hans <hanskrupakar1994@gmail.com>
2023-07-13 17:10:30 +0530

    conv-3-1-1-memorization-mistake

--- ecology_semantic_segmentation/model/deeplabv3plus_depthwise.py
+++ ecology_semantic_segmentation/model/deeplabv3plus_depthwise.py
@@ -5,16 +5,16 @@ class DeepLabV3PlusDepthwise(nn.Module):
 
     def __init__(self, encoder_name, encoder_weights, in_channels, classes, depthwise_multiplier=5):
 
-		self.smp_deeplab_model = smp.DeepLabV3Plus(encoder_name=encoder_name, 
-											encoder_weights=encoder_weights,     
-											in_channels=in_channels,                  
-											classes=classes*depthwise_multiplier)
+        self.smp_deeplab_model = smp.DeepLabV3Plus(encoder_name=encoder_name, 
+                                                    encoder_weights=encoder_weights,     
+                                                    in_channels=in_channels,                  
+                                                    classes=classes*depthwise_multiplier)
 
         self.last_layers = nn.Conv2d(in_channels=classes*depthwise_multiplier, 
                                     out_channels=classes, 
                                     kernel_size=3, 
                                     stride=1, 
-                                    padding=0)
+                                    padding=1)
 
     def forward(self, x):
 
